ffd79e96dfda13eabaef00b3a8f741f09c0c7713
Henrique Junqueira <41648535+henriquejunqueira@users.noreply.github.com>
2024-10-29 17:25:43 -0300

    core module

---
+++ 2_FUNDAMENTOS/3_core_module/index.js
@@ -0,0 +1,5 @@
+const path = require('path'); // import o módulo externo path
+
+const extension = path.extname('arquivo.php'); // verifica qual o tipo de extensão do arquivo
+
+console.log(extension); // exibe qual a extensão
