706dc6c709fbb6705089b6b3c249f771c1338a0c
Henrique Junqueira <41648535+henriquejunqueira@users.noreply.github.com>
2024-10-31 14:21:21 -0300

    Working with the os module to display system information

---
+++ 3_CORE_MODULES/15_modulo_os/index.js
@@ -0,0 +1,6 @@
+const os = require('os');
+
+console.log(os.cpus()); // número de cpus
+console.log(os.freemem()); // quantidade de memória ram livre
+console.log(os.homedir()); // diretório home
+console.log(os.type()); // tipo de SO
