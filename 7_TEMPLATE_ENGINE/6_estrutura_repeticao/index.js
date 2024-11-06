57b548d68d0d9693805c2eec87e6521725f2ae32
Henrique Junqueira <41648535+henriquejunqueira@users.noreply.github.com>
2024-11-06 10:47:12 -0300

    Repetition structure in handlebars

---
+++ 7_TEMPLATE_ENGINE/6_estrutura_repeticao/index.js
@@ -0,0 +1,35 @@
+import express from 'express';
+import { engine } from 'express-handlebars';
+
+const app = express();
+
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+
+app.get('/dashboard', (req, res) => {
+  const items = ['Item A', 'Item B', 'Item C'];
+
+  res.render('dashboard', { items });
+});
+
+app.get('/', (req, res, next) => {
+  const user = {
+    name: 'Henrique',
+    surname: 'Junqueira',
+    age: 30,
+  };
+
+  const palavra = 'Teste';
+
+  const auth = true;
+
+  const approved = true;
+  // const approved = false;
+
+  // res.render('nome_da_view', { atributos_que_serão_impressos_na_view });
+  res.render('home', { user: user, palavra, auth, approved });
+});
+
+app.listen(3000, () => {
+  console.log('App funcionando!');
+});
