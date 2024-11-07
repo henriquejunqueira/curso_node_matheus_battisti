ba922a1746748dcc239cca80dc933d91c077b83a
Henrique Junqueira <41648535+henriquejunqueira@users.noreply.github.com>
2024-11-07 17:00:55 -0300

    Selecting data with filter (WHERE)

---
+++ 8_NODE_E_MYSQL/4_resgatando_individuais/index.js
@@ -0,0 +1,90 @@
+import express from 'express';
+import { engine } from 'express-handlebars';
+import mysql from 'mysql';
+
+const app = express();
+
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+app.use(express.json());
+
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.render('home');
+});
+
+app.post('/books/insertbook', (req, res) => {
+  const title = req.body.title;
+  const pageqty = req.body.pageqty;
+
+  const sql = `INSERT INTO books (title, pageqty) VALUES ('${title}', '${pageqty}')`;
+
+  conn.query(sql, function (err) {
+    if (err) {
+      console.log(err);
+      return;
+    }
+
+    res.redirect('/books');
+  });
+});
+
+app.get('/books', (req, res) => {
+  const sql = 'SELECT * FROM books';
+
+  conn.query(sql, function (err, data) {
+    if (err) {
+      console.log(err);
+      return;
+    }
+
+    const books = data;
+
+    console.log(books);
+
+    res.render('books', { books });
+  });
+});
+
+app.get('/books/:id', (req, res) => {
+  const id = req.params.id;
+
+  const sql = `SELECT * FROM books WHERE id = ${id}`;
+
+  conn.query(sql, function (err, data) {
+    if (err) {
+      console.log(err);
+      return;
+    }
+
+    const book = data[0];
+
+    res.render('book', { book });
+  });
+});
+
+const conn = mysql.createConnection({
+  host: 'localhost',
+  user: 'henrique',
+  password: 'slipknot1994',
+  database: 'db_nodemysql',
+});
+
+conn.connect(function (err) {
+  if (err) {
+    console.log(err);
+    return;
+  }
+
+  app.listen(3000, () => {
+    const port = 3000;
+    console.log(`Conectado ao MySQL e app rodando na porta ${port}...`);
+  });
+});
