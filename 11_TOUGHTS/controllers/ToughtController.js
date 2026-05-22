01935a33e49a789bacf514f8e4c51881b4ba71f3
henriquejunqueira <henriquejunqueira15@gmail.com>
2026-05-21 23:40:19 -0300

    Resgatando Pensamentos do banco de dados para a dashboard

--- 11_TOUGHTS/controllers/ToughtController.js
+++ 11_TOUGHTS/controllers/ToughtController.js
@@ -7,7 +7,23 @@ module.exports = class ToughtController {
   }
 
   static async dashboard(req, res) {
-    res.render('toughts/dashboard');
+    const userId = req.session.userid;
+    const user = await User.findOne({
+      where: {
+        id: userId,
+      },
+      include: Tought, // pega todos os pensamentos do usuário
+      plain: true, // pega só os dados necessários para fazer o forEach no array
+    });
+
+    // check if user exists
+    if (!user) {
+      res.redirect('/login');
+    }
+
+    const toughts = user.Toughts.map((result) => result.dataValues);
+
+    res.render('toughts/dashboard', { toughts });
   }
 
   static createTought(req, res) {
